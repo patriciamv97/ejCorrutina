d516ff1acd50bdbd732d21fdb9aad664cf94f2e4
Patricia <pmaceirasvarela@danielcastelao.org>
2021-10-25 09:10:15 +0200

    Listener del boton y nueva funcion

--- app/src/main/java/com/dam2a/ejcorrutina/MainActivity.kt
+++ app/src/main/java/com/dam2a/ejcorrutina/MainActivity.kt
@@ -2,10 +2,19 @@ package com.dam2a.ejcorrutina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val miBoton : Button = findViewById(R.id.boton)
+        miBoton.setOnClickListener{
+            lanzarCorrutina()
+        }
+
+    }
+    private fun lanzarCorrutina(){
+
     }
 }
