6382bb1bb967fb551d232469ee16c8f8e4a73bce
Patricia <pmaceirasvarela@danielcastelao.org>
2021-10-25 09:14:34 +0200

    Creamos una corrutina

--- app/src/main/java/com/dam2a/ejcorrutina/MainActivity.kt
+++ app/src/main/java/com/dam2a/ejcorrutina/MainActivity.kt
@@ -3,6 +3,9 @@ package com.dam2a.ejcorrutina
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,10 @@ class MainActivity : AppCompatActivity() {
 
     }
     private fun lanzarCorrutina(){
+        //Launch se encarga de crear una corrutina
+        // vamos a poder identificarla con 'job'
+        val job = GlobalScope.launch(Dispatchers.Main){
 
+        }
     }
 }
